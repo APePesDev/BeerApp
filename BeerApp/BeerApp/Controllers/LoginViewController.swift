c87d7bb81f13e0125386079574505e9fc8b6b24f
Eric Martinez Paredes <eric.martinez@unam.mx>
2018-10-20 13:43:53 -0500

    loginview

---
+++ BeerApp/BeerApp/Controllers/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  BeerApp
+//
+//  Created by Eric Martínez Paredes on 20/10/18.
+//  Copyright © 2018 ACL. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+}
---
+++ BeerApp/BeerApp/Controllers/Users.swift
@@ -0,0 +1,20 @@
+//
+//  Users.swift
+//  BeerApp
+//
+//  Created by Eric Martínez Paredes on 20/10/18.
+//  Copyright © 2018 ACL. All rights reserved.
+//
+
+import UIKit
+
+struct Users {
+    
+    var username: String = "username"
+    var password: String = "password"
+    
+    func userlogin() -> Bool {
+        return true
+    }
+    
+}
