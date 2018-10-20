17028cab127fc0ba04446883c32f0e3fa67c84f2
Eric Martinez Paredes <eric.martinez@unam.mx>
2018-10-20 10:02:30 -0500

    First Commit

---
+++ BeerApp/BeerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeerApp
+//
+//  Created by Eric Martínez Paredes on 20/10/18.
+//  Copyright © 2018 ACL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
