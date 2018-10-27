d26ddc51aa1167fabf11c52668ea651f381aa188
angeltex <angel.070594@gmail.com>
2018-10-26 20:56:36 -0500

    segue to ShowDetail

--- BeerApp/BeerApp/Controllers/BeerListViewController.swift
+++ BeerApp/BeerApp/Controllers/BeerListViewController.swift
@@ -55,14 +55,5 @@ class BeerListViewController: UIViewController, UITableViewDataSource, UITableVi
             beerDetailViewController.beerdetail = beer
         }
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
