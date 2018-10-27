c22499d277ba356db45a80b970bf9efacafab2fd
Isaac R <izaak@live.co.kr>
2018-10-26 19:25:52 -0500

    add class BeerCarousel and conection beertableview cell

---
+++ BeerApp/BeerApp/Models/BeerCarousel.swift
@@ -0,0 +1,16 @@
+//
+//  BeerCarousel.swift
+//  BeerApp
+//
+//  Created by Rodriguez Isaac on 10/26/18.
+//  Copyright © 2018 ACL. All rights reserved.
+//
+
+import Foundation
+
+
+class BeerCarousel {
+    var  beers: [Beer] = [Beer(category: 1, name: "Corona", description: "nuinguna", meta: "no", imageName: "order"),
+                          Beer(category: 2, name: "Hazmela Rusa", description: "La mejor", meta: "no", imageName: "order")
+    ]
+}
